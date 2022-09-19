a9e62845e8c240266e7dbfde25dd0a516df82399
liquidGoo <nitor25@gmail.com>
2022-09-19 12:47:09 +0300

    Add local to world and world to view transformations

---
+++ CGA/Matrix4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGA
+{
+    class Matrix4
+    {
+        private const int SIZE = 4;
+        private float[,] matrix = new float[SIZE, SIZE];
+
+
+        public float this[int index1, int index2]
+        {
+            get
+            {
+                return matrix[index1, index2];
+            }
+            set
+            {
+                matrix[index1, index2] = value;
+            }
+        }
+
+        public static Matrix4 operator *(Matrix4 a, Matrix4 b )
+        {
+            Matrix4 matrix = new Matrix4();
+            for (int i = 0; i < SIZE; i++)
+            {
+                for (int j = 0; j < SIZE; j++)
+                {
+                    for (int k = 0; k < SIZE; k++)
+                    {
+                        matrix[i,j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        public static Vector3 operator *(Matrix4 matrix, Vector3 vector)
+        {
+            Matrix4 vectorMatrix = new Matrix4();
+            for (int i = 0; i < 3; i++)
+            {
+                vectorMatrix[i, 0] = vector[i];
+            }
+            vectorMatrix[3, 0] = 1;
+
+            Matrix4 resultMatrix =  matrix * vectorMatrix;
+
+            return new Vector3(resultMatrix[0, 0], resultMatrix[1, 0], resultMatrix[2, 0]);
+        }
+
+        public static Matrix4 One()
+        {
+            Matrix4 matrix = new Matrix4();
+            for (int i = 0; i < SIZE; i++)
+            {
+                matrix[i,i] = 1;
+            }
+            return matrix;
+        }
+    }
+}
--- CGA/Vector3.cs
+++ CGA/Vector3.cs
@@ -14,6 +14,55 @@ namespace CGA
 
         private float w;
 
+        public float this[int index]
+        {
+            get
+            {
+                switch (index)
+                {
+                    case 0:
+                        return x;
+                    case 1:
+                        return y;
+                    case 2: 
+                        return z;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+            set
+            {
+                switch (index)
+                {
+                    case 0:
+                        x = value;
+                        break;
+                    case 1:
+                        y = value;
+                        break;
+                    case 2:
+                        z = value;
+                        break;
+                    default:
+                        throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public float magnitude
+        {
+            get
+            {
+                return MathF.Sqrt(x * x + y * y + z * z);
+            }
+        }
+        public Vector3 normalized
+        {
+            get
+            {
+                return this / magnitude;
+            }
+        }
 
         public static Vector3 operator +(Vector3 a, Vector3 b)
         {
@@ -30,24 +79,124 @@ namespace CGA
             return a + (-b);
         }
 
+        public static Vector3 operator *(Vector3 a, float b)
+        {
+            return new Vector3(a.x * b, a.y * b, a.z * b);
+        }
         public static float operator *(Vector3 a, Vector3 b)
         {
-            return a.x * b.x + a.y * b.y + a.z * b.z;
+            return a.Dot(b);
         }
 
-        public static float DotProduct(Vector3 a, Vector3 b)
+        public static Vector3 operator /(Vector3 a, float b)
         {
-            return a * b;
+            return a * (1 / b);
         }
 
-        public float DotProduct(Vector3 other)
+
+        public float Dot(Vector3 other)
         {
-            return DotProduct(this, other);
+            return x * other.x + y * other.y + z * other.z;
+        }
+
+        public  Vector3 Cross(Vector3 other)
+        {
+            return new Vector3(y * other.z - z * other.y,
+                z * other.x - x * other.z,
+                x * other.y - y * other.x);
+        }
+        public Vector3 Scale(Vector3 scale)
+        {
+            return new Vector3(x * scale.x, y * scale.y, z * scale.z);
+        }
+        
+        public Vector3 RotateX(float angle)
+        {
+            Matrix4 matrix = Matrix4.One();
+            float cos = MathF.Cos(angle);
+            float sin = MathF.Sin(angle);
+            matrix[1, 1] = cos;
+            matrix[1, 2] = -sin;
+            matrix[2, 1] = sin;
+            matrix[2, 2] = cos;
+            return matrix * this;
         }
 
 
-        
-        
+        public Vector3 RotateY(float angle)
+        {
+            Matrix4 matrix = Matrix4.One();
+            float cos = MathF.Cos(angle);
+            float sin = MathF.Sin(angle);
+            matrix[0, 0] = cos;
+            matrix[0, 2] = sin;
+            matrix[2, 0] = -sin;
+            matrix[2, 2] = cos;
+            return matrix * this;
+        }
+
+        public Vector3 RotateZ(float angle)
+        {
+            Matrix4 matrix = Matrix4.One();
+            float cos = MathF.Cos(angle);
+            float sin = MathF.Sin(angle);
+            matrix[0, 0] = cos;
+            matrix[0, 1] = -sin;
+            matrix[1, 0] = sin;
+            matrix[1, 1] = cos;
+            return matrix * this;
+        }
+
+        public Vector3 LocalToWorld(Vector3 xAxis, Vector3 yAxis, Vector3 zAxis, Vector3 translation)
+        {
+            Matrix4 matrix = Matrix4.One();
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[i, 0] = xAxis[i];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[i,1] = yAxis[i];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[i,2] = zAxis[i];
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[i,3] = translation[i];
+            }
+            return matrix * this;
+        }
+
+        public Vector3 WorldToView(Vector3 eye, Vector3 target, Vector3 up)
+        {
+            Vector3 zAxis = (target - eye).normalized;
+            Vector3 xAxis = (up.Cross(zAxis)).normalized;
+            Vector3 yAxis = up;
+
+            Matrix4 matrix = Matrix4.One();
+
+            for (int i =0; i < 3; i++)
+            {
+                matrix[0, i] = xAxis[i];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[1, i] = yAxis[i];
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                matrix[2, i] = zAxis[i];
+            }
+            matrix[0, 3] = -xAxis * eye;
+            matrix[1, 3] = -yAxis * eye;
+            matrix[2, 3] = -zAxis * eye;
+
+            return matrix * this;
+        }
 
         public Vector3(float x, float y, float z)
         {
